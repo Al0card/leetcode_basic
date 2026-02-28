d4a0a656696bced5b57e68932ebb7faa4b911273
TheWeaver0 <weaver0thedaem0n@gmail.com>
2026-02-28 19:46:46 +0000

    find all numbers dissapeared in an array

--- a.py
+++ a.py
@@ -1,2 +1,21 @@
-listo = [1, 2, 3, 4]
-print(sum(listo))
+nums = [4,3,2,7,8,2,3,1]
+
+class Solution:
+    def findDisappearedNumbers(self, nums) :
+        seto = set(nums)
+        listo = sorted(seto)
+        tempo = 0
+        r = []
+        for x in listo:
+            if x == tempo+1:
+                tempo = x
+            else:
+                # print(tempo)
+                while tempo < (x-1):
+                    # print(f"{tempo}, {x-1}, {tempo < (x-1)}")
+                    tempo = tempo+1
+                    r.append(tempo)  
+                tempo = tempo+1
+        return r 
+s = Solution
+print(s.findDisappearedNumbers(s, nums))
